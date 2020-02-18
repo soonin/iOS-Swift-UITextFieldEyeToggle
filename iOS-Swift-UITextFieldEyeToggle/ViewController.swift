cd9a4485ba6f99ab477e95969b56381ea8f8dcd4
Pooya Hatami <am.hatami@gmail.com>
2020-02-18 11:57:13 -0800

    Initial Commit

---
+++ iOS-Swift-UITextFieldEyeToggle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Swift-UITextFieldEyeToggle
+//
+//  Created by Pooya on 2020-02-18.
+//  Copyright © 2020 centurytrail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
