c9bf31c754a3719ad2f60eed217c7ad68614bd97
Pooya Hatami <am.hatami@gmail.com>
2020-02-18 16:42:03 -0800

    first commit

--- iOS-Swift-UITextFieldEyeToggle/ViewController.swift
+++ iOS-Swift-UITextFieldEyeToggle/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var password_textfield : UITextField!
+    let button = UIButton(type: .custom)
+    var btnColor = UIButton(type: .custom)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //password_textfield.rightView = btnColor
+        password_textfield.rightViewMode = .unlessEditing
+        
+        button.setImage(UIImage(named: "eye_closed.png"), for: .normal)
+        button.imageEdgeInsets = UIEdgeInsets(top: 5, left: -24, bottom: 5, right: 15)
+        button.frame = CGRect(x: CGFloat(password_textfield.frame.size.width - 25), y: CGFloat(5), width: CGFloat(15), height: CGFloat(25))
+        button.addTarget(self, action: #selector(self.btnPasswordVisiblityClicked), for: .touchUpInside)
+        password_textfield.rightView = button
+        password_textfield.rightViewMode = .always
+        
     }
 
+    @IBAction func btnPasswordVisiblityClicked(_ sender: Any) {
+        (sender as! UIButton).isSelected = !(sender as! UIButton).isSelected
+        if (sender as! UIButton).isSelected {
+            self.password_textfield.isSecureTextEntry = false
+            button.setImage(UIImage(named: "eye_open.png"), for: .normal)
+        } else {
+            self.password_textfield.isSecureTextEntry = true
+            button.setImage(UIImage(named: "eye_closed.png"), for: .normal)
+        }
+    }
+    
 
 }
 
